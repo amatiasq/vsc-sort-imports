5ff45e31885da8645064b9a534dad409d7c03b99
Jason Quense <monastic.panic@gmail.com>
2019-07-12 15:12:50 -0400

    chore: upgrade import sort

--- src/sort.ts
+++ src/sort.ts
@@ -6,19 +6,18 @@ import onSave from './on-save';
 import { getConfiguration, getMaxRange } from './utils';
 
 // const findImports = /^import [^\n]+\n+/gm;
-const defaultLanguages = [
-    'javascript',
-    'typescript',
-];
+const defaultLanguages = ['javascript', 'typescript'];
 
 let cachedParser: string;
 let cachedStyle: string;
 let cachedOptions: object;
 
-
 export function sort(document: TextDocument): string {
-    const languages = getConfiguration<string[]>('languages') || defaultLanguages;
-    const isValidLanguage = languages.some(language => document.languageId.includes(language));
+    const languages =
+        getConfiguration<string[]>('languages') || defaultLanguages;
+    const isValidLanguage = languages.some(language =>
+        document.languageId.includes(language)
+    );
 
     if (!isValidLanguage) {
         return;
@@ -29,27 +28,39 @@ export function sort(document: TextDocument): string {
     const extension = extname(fileName);
     const directory = dirname(fileName);
 
-    let result;
     const config = clone(DEFAULT_CONFIGS);
     const defaultSortStyle = getConfiguration<string>('default-sort-style');
 
-    for (const languages of Object.keys(config)) {
-        config[languages].style = defaultSortStyle;
-    }
+    Object.keys(config).forEach(key => {
+        config[key].style = require.resolve(
+            `import-sort-style-${defaultSortStyle}`
+        );
+    });
 
-    const useCache = getConfiguration<boolean>('cache-package-json-config-checks');
+    const useCache = getConfiguration<boolean>(
+        'cache-package-json-config-checks'
+    );
 
     try {
         if (!useCache || !cachedParser) {
-            const { parser, style, config: { options } } = getConfig(extension, directory, config);
+            const {
+                parser,
+                style,
+                config: { options }
+            } = getConfig(extension, directory, config);
             cachedParser = parser;
             cachedStyle = style;
             cachedOptions = options;
         }
 
-        const result = importSort(currentText, cachedParser, cachedStyle, fileName, cachedOptions);
+        const result = importSort(
+            currentText,
+            cachedParser,
+            cachedStyle,
+            fileName,
+            cachedOptions
+        );
         return result.code;
-
     } catch (exception) {
         if (!getConfiguration<boolean>('suppress-warnings')) {
             window.showWarningMessage(`Error sorting imports: ${exception}`);
@@ -58,7 +69,6 @@ export function sort(document: TextDocument): string {
         return null;
     }
 }
-
 
 export function sortCurrentDocument() {
     const {
@@ -74,12 +84,10 @@ export function sortCurrentDocument() {
     return editor.edit(edit => edit.replace(getMaxRange(), sortedText));
 }
 
-
 export async function saveWithoutSorting() {
     const { document } = window.activeTextEditor;
     onSave.bypass(async () => await document.save());
 }
-
 
 function clone(object) {
     return JSON.parse(JSON.stringify(object));
