8c740e00f9e15df2016f7ea8eeab5f1e96114079
Jason Quense <monastic.panic@gmail.com>
2019-08-06 08:35:51 -0400

    chore: add prettier and lint staging

--- src/on-save.ts
+++ src/on-save.ts
@@ -1,81 +1,77 @@
 import {
-    Disposable,
-    TextDocumentWillSaveEvent,
-    workspace,
-    window,
-    TextEditorEdit,
-    TextDocument,
-    TextEdit,
+  Disposable,
+  TextDocumentWillSaveEvent,
+  workspace,
+  window,
+  TextEditorEdit,
+  TextDocument,
+  TextEdit
 } from 'vscode';
 import { sort } from './sort';
 import { getConfiguration, getMaxRange } from './utils';
 
 let subscription: Disposable;
 
-
 export default {
+  get isEnabled() {
+    return getConfiguration<boolean>('on-save');
+  },
 
-    get isEnabled() {
-        return getConfiguration<boolean>('on-save');
-    },
-
-    register() {
-        if (subscription) {
-            return;
-        }
-
-        subscription = workspace.onWillSaveTextDocument(listener);
-    },
-
-    unregister() {
-        if (!subscription) {
-            return;
-        }
-
-        subscription.dispose();
-        subscription = null;
-    },
-
-    update() {
-        if (this.isEnabled) {
-            this.register();
-        } else {
-            this.unregister();
-        }
-    },
-
-    bypass(action) {
-        this.unregister();
-        const result = action();
-        return result.then((res) => {
-            this.update();
-            return res;
-        });
+  register() {
+    if (subscription) {
+      return;
     }
+
+    subscription = workspace.onWillSaveTextDocument(listener);
+  },
+
+  unregister() {
+    if (!subscription) {
+      return;
+    }
+
+    subscription.dispose();
+    subscription = null;
+  },
+
+  update() {
+    if (this.isEnabled) {
+      this.register();
+    } else {
+      this.unregister();
+    }
+  },
+
+  bypass(action) {
+    this.unregister();
+    const result = action();
+    return result.then(res => {
+      this.update();
+      return res;
+    });
+  }
 };
 
-
 function listener({ document, waitUntil }: TextDocumentWillSaveEvent) {
-    const sortedText = sort(document);
-    if (!sortedText) {
-        return;
-    }
+  const sortedText = sort(document);
+  if (!sortedText) {
+    return;
+  }
 
-    waitUntil(changeContentOfDocument(document, sortedText));
+  waitUntil(changeContentOfDocument(document, sortedText));
 }
 
 function changeContentOfDocument(document: TextDocument, sortedText) {
-    const editor = window.activeTextEditor;
-    const savingActiveDocument = document === editor.document;
+  const editor = window.activeTextEditor;
+  const savingActiveDocument = document === editor.document;
 
-    const maxRange = getMaxRange();
+  const maxRange = getMaxRange();
 
-    if (savingActiveDocument) {
-        return editor.edit((edit: TextEditorEdit) => {
-            edit.replace(maxRange, sortedText);
-        });
-    }
+  if (savingActiveDocument) {
+    return editor.edit((edit: TextEditorEdit) => {
+      edit.replace(maxRange, sortedText);
+    });
+  }
 
-    return Promise.resolve([new TextEdit(maxRange, sortedText)]);
-};
-
+  return Promise.resolve([new TextEdit(maxRange, sortedText)]);
+}
--- src/sort.ts
+++ src/sort.ts
@@ -13,82 +13,81 @@ let cachedStyle: string;
 let cachedOptions: object;
 
 export function sort(document: TextDocument): string {
-    const languages =
-        getConfiguration<string[]>('languages') || defaultLanguages;
-    const isValidLanguage = languages.some(language =>
-        document.languageId.includes(language)
-    );
+  const languages = getConfiguration<string[]>('languages') || defaultLanguages;
+  const isValidLanguage = languages.some(language =>
+    document.languageId.includes(language)
+  );
 
-    if (!isValidLanguage) {
-        return;
+  if (!isValidLanguage) {
+    return;
+  }
+
+  const currentText = document.getText();
+  const fileName = document.fileName;
+  const extension = extname(fileName);
+  const directory = dirname(fileName);
+
+  const config = clone(DEFAULT_CONFIGS);
+  const defaultSortStyle = getConfiguration<string>('default-sort-style');
+
+  Object.keys(config).forEach(key => {
+    config[key].style = require.resolve(
+      `import-sort-style-${defaultSortStyle}`
+    );
+  });
+
+  const useCache = getConfiguration<boolean>(
+    'cache-package-json-config-checks'
+  );
+
+  try {
+    if (!useCache || !cachedParser) {
+      const {
+        parser,
+        style,
+        config: { options }
+      } = getConfig(extension, directory, config);
+      cachedParser = parser;
+      cachedStyle = style;
+      cachedOptions = options;
     }
 
-    const currentText = document.getText();
-    const fileName = document.fileName;
-    const extension = extname(fileName);
-    const directory = dirname(fileName);
-
-    const config = clone(DEFAULT_CONFIGS);
-    const defaultSortStyle = getConfiguration<string>('default-sort-style');
-
-    Object.keys(config).forEach(key => {
-        config[key].style = require.resolve(
-            `import-sort-style-${defaultSortStyle}`
-        );
-    });
-
-    const useCache = getConfiguration<boolean>(
-        'cache-package-json-config-checks'
+    const result = importSort(
+      currentText,
+      cachedParser,
+      cachedStyle,
+      fileName,
+      cachedOptions
     );
-
-    try {
-        if (!useCache || !cachedParser) {
-            const {
-                parser,
-                style,
-                config: { options }
-            } = getConfig(extension, directory, config);
-            cachedParser = parser;
-            cachedStyle = style;
-            cachedOptions = options;
-        }
-
-        const result = importSort(
-            currentText,
-            cachedParser,
-            cachedStyle,
-            fileName,
-            cachedOptions
-        );
-        return result.code;
-    } catch (exception) {
-        if (!getConfiguration<boolean>('suppress-warnings')) {
-            window.showWarningMessage(`Error sorting imports: ${exception}`);
-        }
-
-        return null;
+    return result.code;
+  } catch (exception) {
+    if (!getConfiguration<boolean>('suppress-warnings')) {
+      window.showWarningMessage(`Error sorting imports: ${exception}`);
     }
+
+    return null;
+  }
 }
 
 export function sortCurrentDocument() {
-    const {
-        activeTextEditor: editor,
-        activeTextEditor: { document }
-    } = window;
+  const {
+    activeTextEditor: editor,
+    activeTextEditor: { document }
+  } = window;
 
-    const sortedText = sort(document);
-    if (!sortedText) {
-        return;
-    }
+  const sortedText = sort(document);
+  if (!sortedText) {
+    return;
+  }
 
-    return editor.edit(edit => edit.replace(getMaxRange(), sortedText));
+  return editor.edit(edit => edit.replace(getMaxRange(), sortedText));
 }
 
 export async function saveWithoutSorting() {
-    const { document } = window.activeTextEditor;
-    onSave.bypass(async () => await document.save());
+  const { document } = window.activeTextEditor;
+  onSave.bypass(async () => await document.save());
 }
 
 function clone(object) {
-    return JSON.parse(JSON.stringify(object));
+  return JSON.parse(JSON.stringify(object));
 }
