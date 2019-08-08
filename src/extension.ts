0cef5e9b13a59ddeab92faccf8092749ccedaac4
Jason Quense <monastic.panic@gmail.com>
2019-08-08 07:54:51 -0400

    chore: add prettier and lint staging (#49)

--- src/extension.ts
+++ src/extension.ts
@@ -1,18 +1,21 @@
-import {
-    commands,
-    ExtensionContext,
-    workspace,
-} from 'vscode';
+import { commands, ExtensionContext, workspace } from 'vscode';
 import onSave from './on-save';
 import { saveWithoutSorting, sortCurrentDocument } from './sort';
 import { EXTENSION_NAME } from './utils';
 
 export function activate({ subscriptions }: ExtensionContext) {
-    subscriptions.push(commands.registerCommand(EXTENSION_NAME + '.sort', sortCurrentDocument));
-    subscriptions.push(commands.registerCommand(EXTENSION_NAME + '.save-without-sorting', saveWithoutSorting));
+  subscriptions.push(
+    commands.registerCommand(EXTENSION_NAME + '.sort', sortCurrentDocument)
+  );
+  subscriptions.push(
+    commands.registerCommand(
+      EXTENSION_NAME + '.save-without-sorting',
+      saveWithoutSorting
+    )
+  );
 
-    onSave.update();
-    workspace.onDidChangeConfiguration(() => onSave.update());
+  onSave.update();
+  workspace.onDidChangeConfiguration(() => onSave.update());
 }
 
-export function deactivate() { }
+export function deactivate() {}
--- src/utils.ts
+++ src/utils.ts
@@ -2,12 +2,10 @@ import { Range, workspace } from 'vscode';
 
 export const EXTENSION_NAME = 'sort-imports';
 
-
 export function getConfiguration<T>(key: string): T {
-    return workspace.getConfiguration(EXTENSION_NAME).get<T>(key);
+  return workspace.getConfiguration(EXTENSION_NAME).get<T>(key);
 }
 
-
 export function getMaxRange(): Range {
-    return new Range(0, 0, Number.MAX_VALUE, Number.MAX_VALUE);
+  return new Range(0, 0, Number.MAX_VALUE, Number.MAX_VALUE);
 }
